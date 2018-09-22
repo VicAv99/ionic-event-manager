0533023d499d236b899140abd77dcb914307d95d
Victor Avila <victoravila2123@gmail.com>
2018-09-22 16:17:44 -0700

    signup page

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { LoadingController, AlertController } from '@ionic/angular';
+
+import { AuthService } from '../../services/user/auth.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
-  styleUrls: ['./signup.page.scss'],
+  styleUrls: ['./signup.page.scss']
 })
 export class SignupPage implements OnInit {
+  public signupForm: FormGroup;
+  public loading: any;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.initForm();
   }
 
+  async signupUser(signupForm: FormGroup): Promise<void> {
+    if (signupForm.valid) {
+      const email: string = signupForm.value.email;
+      const password: string = signupForm.value.password;
+
+      this.authService.signupUser(email, password).then(
+        () => {
+          this.loading.dismiss().then(() => {
+            this.router.navigateByUrl('home');
+          });
+        },
+        error => {
+          this.loading.dismiss().then(async () => {
+            const alert = await this.alertCtrl.create({
+              message: error.message,
+              buttons: [{ text: 'Ok', role: 'cancel' }]
+            });
+            await alert.present();
+          });
+        }
+      );
+      this.loading = await this.loadingCtrl.create();
+      await this.loading.present();
+    }
+  }
+
+  initForm() {
+    this.signupForm = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.compose([Validators.minLength(6), Validators.required])]
+    });
+  }
 }
