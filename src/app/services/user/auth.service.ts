75009494ec5ccef1eebdc03f9a9b71843344eac9
Victor Avila <victoravila2123@gmail.com>
2018-09-22 15:31:29 -0700

    auth service

--- src/app/services/user/auth.service.ts
+++ src/app/services/user/auth.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app'; import 'firebase/auth';
+import 'firebase/firestore';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,33 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  loginUser(email: string, password: string): Promise<any> {
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+  }
+
+  logoutUser(): Promise<void> {
+    return firebase.auth().signOut();
+  }
+
+  resetPassword(email: string): Promise<void> {
+    return firebase.auth().sendPasswordResetEmail(email);
+  }
+
+  signupUser(email: string, password: string): Promise<any> {
+    return firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(newUserCredential => {
+        firebase
+          .firestore()
+          .doc(`/userProfile/${newUserCredential.user.uid}`)
+          .set({ email });
+      })
+      .catch(err => {
+        console.error('error', err);
+        throwError(err);
+      });
+  }
+
 }
