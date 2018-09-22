ee9d87b30defe772c542cb88eae20ee498ccbfe7
Victor Avila <victoravila2123@gmail.com>
2018-09-22 16:07:48 -0700

    reset pw page

--- src/app/pages/reset-password/reset-password.page.ts
+++ src/app/pages/reset-password/reset-password.page.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { AlertController } from '@ionic/angular';
+
+import { AuthService } from '../../services/user/auth.service';
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.page.html',
-  styleUrls: ['./reset-password.page.scss'],
+  styleUrls: ['./reset-password.page.scss']
 })
 export class ResetPasswordPage implements OnInit {
+  public resetPasswordForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private alertCtrl: AlertController,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  resetPassword(resetPasswordForm: FormGroup) {
+    if (resetPasswordForm.valid) {
+      const email = resetPasswordForm.value.email;
+
+      this.authService.resetPassword(email).then(
+        async () => {
+          const alert = await this.alertCtrl.create({
+            message: 'Check your email for a password reset link',
+            buttons: [{
+              text: 'Ok',
+              role: 'cancel',
+              handler: () => {
+                this.router.navigateByUrl('login');
+              },
+            },
+            ],
+          });
+          await alert.present();
+        },
+        async error => {
+          const errorAlert = await this.alertCtrl.create({
+            message: error.message,
+            buttons: [{ text: 'Ok', role: 'cancel' }],
+          });
+          await errorAlert.present();
+        }
+      );
+    }
   }
 
+  initForm() {
+    this.resetPasswordForm = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email]) ]
+    });
+  }
 }
